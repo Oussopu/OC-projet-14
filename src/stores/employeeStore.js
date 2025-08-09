d9351471a0c8bff19d79edb3dc4d22cd94ab2794
oussopu <o.haffaf.dev@gmail.com>
2025-08-09 10:24:23 +0200

    fix: README and the name of project

--- src/stores/employeeStore.js
+++ src/stores/employeeStore.js
@@ -1,20 +1,12 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-const useEmployeeStore = create(
-  persist(
-    (set) => ({
-      employees: [],
-      addEmployee: (employee) =>
-        set((state) => ({
-          employees: [...state.employees, employee],
-        })),
-      setEmployees: (employees) => set({ employees }),
-    }),
-    {
-      name: "employee-storage",
-    },
-  ),
-);
+const useEmployeeStore = create((set) => ({
+  employees: [],
+  addEmployee: (employee) =>
+    set((state) => ({
+      employees: [...state.employees, employee],
+    })),
+  setEmployees: (employees) => set({ employees }),
+}));
 
 export default useEmployeeStore;
